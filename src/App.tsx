4ff3d7a51ebf830366dda1b89b8e8d1a19383882
Giulia Soares <giulialeticiasoares.com>
2025-08-29 20:24:19 -0300

    configurate the repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home } from "./pages/home";
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
